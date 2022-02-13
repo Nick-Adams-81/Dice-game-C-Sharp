76ab86b281184055b8a48c89e4d549a89263cf21
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-13 10:34:21 -0600

    completed game

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -9,6 +9,10 @@ namespace DiceGame
 
             int playerRandomNum;
             int cpuRandomNum;
+
+            int playerPoints = 0;
+            int cpuPoints = 0;
+
             Random random = new Random();
             for(int i = 0; i < 10; i++)
             {
@@ -18,9 +22,39 @@ namespace DiceGame
                 playerRandomNum = random.Next(1, 7);
                 Console.WriteLine("You rolled a " + playerRandomNum);
 
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(700);
+
                 cpuRandomNum = random.Next(1, 7);
                 Console.WriteLine("The cpu rolled a " + cpuRandomNum);
 
+                if(playerRandomNum > cpuRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("player wins this round");
+                } else if(playerRandomNum < cpuRandomNum)
+                {
+                    cpuPoints++;
+                    Console.WriteLine("cpu wins this round");
+                } else
+                {
+                    Console.WriteLine("you and the cpu rolled the same nuimber, it's a draw!");
+                }
+                Console.WriteLine("The score is now - Player: " + playerPoints + " CPU: " + cpuPoints + ".");
+                Console.WriteLine();
+
+
+            }
+
+            if(playerPoints > cpuPoints)
+            {
+                Console.WriteLine("You win!!");
+            } else if (playerPoints < cpuPoints)
+            {
+                Console.WriteLine("The cpu wins!");
+            } else
+            {
+                Console.WriteLine("It's a tie!!!");
             }
 
             Console.ReadKey();
