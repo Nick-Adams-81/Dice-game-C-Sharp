437b8b1f30dfe18bb1b9ef442b052b28d3624dd8
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-13 10:26:10 -0600

    added basic game functioning

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -6,7 +6,24 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int playerRandomNum;
+            int cpuRandomNum;
+            Random random = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice!");
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a " + playerRandomNum);
+
+                cpuRandomNum = random.Next(1, 7);
+                Console.WriteLine("The cpu rolled a " + cpuRandomNum);
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
